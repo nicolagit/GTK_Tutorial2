cebc855e9b68c440f0ca69e5e936da4661a8077c
Nicola <nyc.zyp@gmail.com>
2016-07-03 22:20:02 +0100

    GTK+ tutorial 2 - label

--- first.cpp
+++ first.cpp
@@ -3,10 +3,13 @@
 int main(int argc,char* argv[])
 {
         gtk_init(&argc,&argv);
-        GtkWidget* window;
+        GtkWidget *window, *label;
         window=gtk_window_new(GTK_WINDOW_TOPLEVEL);
+        label=gtk_label_new("Hello world");
+        gtk_label_set_text(GTK_LABEL(label),"World hello");
         g_signal_connect(window,"delete-event",G_CALLBACK(gtk_main_quit),NULL);
-        gtk_widget_show(window);
+        gtk_container_add(GTK_CONTAINER(window), label);
+        gtk_widget_show_all(window);
         gtk_main();
         return 0;
 }
