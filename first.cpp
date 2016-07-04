b6004fe0314b58c9e527c1bab8f98ad509eb5a33
Nicola <nyc.zyp@gmail.com>
2016-07-04 13:04:16 +0100

    GTK+ tutorial 3 - button (part 2)

--- first.cpp
+++ first.cpp
@@ -11,6 +11,7 @@ int main(int argc,char* argv[])
         g_signal_connect(button,"clicked",G_CALLBACK(gtk_main_quit),NULL);
         gtk_container_add(GTK_CONTAINER(window),button);
         gtk_widget_set_size_request(window,300, 300);
+        gtk_window_set_title(GTK_WINDOW(window),"Hello world");
         gtk_widget_show_all(window);
         gtk_main();
         return 0;
