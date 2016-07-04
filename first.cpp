9e688356e3f0b29afa629e84147be3ae4ac071ef
Nicola <nyc.zyp@gmail.com>
2016-07-04 12:34:32 +0100

    GTK+ tutorial 3 - button (part 1)

--- first.cpp
+++ first.cpp
@@ -1,18 +1,14 @@
 #include <gtk/gtk.h>
 
-static void button_clicked(GtkWidget* widget,gpointer data)
-{
-        g_print("Button is pressed\n");
-}
 
 int main(int argc,char* argv[])
 {
         gtk_init(&argc,&argv);
         GtkWidget *window,*button;
         window=gtk_window_new(GTK_WINDOW_TOPLEVEL);
-        button=gtk_button_new_with_label("Click me");
+        button=gtk_button_new_with_label("Exit");
         g_signal_connect(window,"delete_event",G_CALLBACK(gtk_main_quit),NULL);
-        g_signal_connect(button,"clicked",G_CALLBACK(button_clicked),NULL);
+        g_signal_connect(button,"clicked",G_CALLBACK(gtk_main_quit),NULL);
         gtk_container_add(GTK_CONTAINER(window),button);
         gtk_widget_show_all(window);
         gtk_main();
